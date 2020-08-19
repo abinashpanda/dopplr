e339dcd86786d327d34778329090030223dd8141
Abinash Panda <abinash@prodios.com>
2020-08-19 13:38:07 +0530

    Install nestsjs cli if not present

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ ! "$(which nest)" ]; then
+  npm install -g @nestjs/cli
+fi
+
 if [ ! -d "node_modules" ] || [ ! "$(ls -A node_modules)" ]; then
   echo "Node modules not installed. Installing..."
   yarn install
